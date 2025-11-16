194fb93e4adee9fcd4eed418aed0b137041d29a9
Ahmed Riyad <ahmedriyad407@gmail.com>
2025-11-16 18:15:41 +0200

    chore(architecture): set up project architecture (N-Tier) clean architecture chore(Program.cs): configure Swagger for documentation

--- HireAI.API/Program.cs
+++ HireAI.API/Program.cs
@@ -13,12 +13,21 @@ namespace HireAI.API
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Add Swagger generation service
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+                app.UseSwaggerUI();
             }
 
             app.UseAuthorization();
