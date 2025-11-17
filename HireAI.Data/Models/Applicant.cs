9127cf74117d706649806b21f5ebddd99de8a1a7
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-11-17 20:06:40 +0200

    feat(domain):implemented Applicant and Recruiter model

---
+++ HireAI.Data/Models/Applicant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Data.Models
+{
+    public class Applicant: User
+    {
+        public string ResumeUrl { get; set; } = null!;
+       
+    }
+}
---
+++ HireAI.Data/Models/Recruiter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Data.Models
+{
+    public class Recruiter
+    {
+        public string CompanyName { get; set; } = null!;
+    }
+}
