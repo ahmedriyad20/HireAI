2ddfe419059d6ab6fc97b662613eef4b051a0fc9
Hamza Al-Gohary <hamzaelgohary15_18@yahoo.com>
2025-11-17 19:01:33 +0200

    feat(domain): added TestAttempt model

--- HireAI.Data/Models/TestAttempt.cs
+++ HireAI.Data/Models/TestAttempt.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HireAI.Data.Models
 {
-    internal class TestAttempt
+    public class TestAttempt
     {
+        public Guid Id { get; set; }
+        public Guid JobId { get; set; }
+        public Guid ApplicationId { get; set; }
+        public Guid TestId { get; set; }
+        public string CreatedAt { get; set; }
+
     }
 }
