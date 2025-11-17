396f926894d4c98d34e78ccd97060e14c516377f
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-11-17 13:43:41 +0200

    feat(domain):implemented QuestionEvaluation model

---
+++ HireAI.Data/Models/QuestionEvaluation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Data.Models
+{
+    public class QuestionEvaluation
+    {
+        public Guid Id { get; set; }
+
+        public Guid ResponseId { get; set; } // Foreign Key to Response
+
+        public bool IsvCorrect { get; set; } = false;
+
+        public  string Feedback { get; set; } = null!;
+
+        public DateTime EvaluatedAt { get; set; }
+    }
+}
