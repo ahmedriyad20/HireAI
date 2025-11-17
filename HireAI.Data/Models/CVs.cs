15ac122faf14016551162660a88fba4c6eb50e6a
Hamza Al-Gohary <hamzaelgohary15_18@yahoo.com>
2025-11-17 18:44:02 +0200

    feat(domain): added CVs model

--- HireAI.Data/Models/CVs.cs
+++ HireAI.Data/Models/CVs.cs
@@ -10,36 +10,17 @@ namespace HireAI.Data.Models
 {
     internal class CVs
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Required]
-        [MaxLength(50)]
-        public string Id { get; set; }
-
-        [MaxLength(20)]
+        public Guid Id { get; set; }
         public string? Phone { get; set; }
-
-        [MaxLength(255)]
         public string? LinkedInPath { get; set; }
-
-        [MaxLength(255)]
         public string? GitHubPath { get; set; }
-
-        [MaxLength(100)]
         public string? Title { get; set; }
-
-        [MaxLength(500)]
         public string? Education { get; set; }
-
-        [MaxLength(1000)]
         public string? Experience { get; set; }
-
         public float? YearOfExperience { get; set; }
-
-        [MaxLength(500)]
         public string? Certifications { get; set; }
 
         // Foreign Key
-        public int? CandidatedId { get; set; }
+        public Guid CandidatedId { get; set; }
     }
 }
