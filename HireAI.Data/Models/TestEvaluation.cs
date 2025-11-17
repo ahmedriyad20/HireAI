d750ce6ab724e8c27805b90a46368ccbc6caee56
Hamza Al-Gohary <hamzaelgohary15_18@yahoo.com>
2025-11-17 19:04:09 +0200

    feat(domain): added TestEvaluation model

--- HireAI.Data/Models/TestEvaluation.cs
+++ HireAI.Data/Models/TestEvaluation.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HireAI.Data.Models
 {
-    internal class TestEvaluation
+    public class TestEvaluation
     {
+        public Guid Id { get; set; }
+        public Guid TestAttemptId { get; set; }
+        public float TotalScore { get; set; }
+        public float MaxTotal { get; set; }
+        public bool Passed { get; set; }
+        public int Status { get; set; }
+        public int EvaluatedAt { get; set; }
     }
 }
