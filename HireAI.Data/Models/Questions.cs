7580676b3011f88d564a10d9be72eb5e88fcc715
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-11-17 13:33:11 +0200

    feat(domain): implemented Question Model and QuestionAnswers enum

---
+++ HireAI.Data/Models/Questions.cs
@@ -0,0 +1,26 @@
+﻿using HireAI.Data.Helpers.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Data.Models
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public Guid TestId { get; set; } //foreign key to Test
+        public string QuestionText { get; set; } = null!;
+        public QuestionAnswers? Answer { get; set; }
+
+        public int QuestionNumber { get; set; }
+        private Question() { }
+        public Question(Guid testId, string questionText, QuestionAnswers  answer )
+        {
+            TestId = testId;
+            QuestionText = questionText;
+            Answer = answer;
+        }   
+    }
+}
